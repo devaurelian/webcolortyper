fad220f5d87eed60f8de7f2c4bd11b3f884bc809
techaurelian <techaurelian@users.noreply.github.com>
2020-08-18 11:22:38 +0000

    deploy: aa7c36e51a227f694b87015ff0ca1e53947d97e9

---
+++ sw.js
@@ -0,0 +1,40 @@
+/*
+ *
+ *  Web Color Typer
+ *  https://github.com/techaurelian/webcolortyper
+ * 
+ *  Copyright (c) 2020 techaurelian. All rights reserved. 
+ *  Licensed under the MIT license.
+ *
+ */
+
+const version = "1";
+const cacheName = `webcolortyper-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/css/modern-normalize.css`,
+        `/css/main.css`,
+        `/js/main.js`
+      ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
